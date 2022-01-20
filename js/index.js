89ea94dbf6a2ffb53133d0f79638f925215992c9
RafaelvBossio <rafaelvargasbossio99@gmail.com>
2022-01-20 15:25:15 -0500

    las funciones encriptar y desencriptar ya fueron completadas

--- js/index.js
+++ js/index.js
@@ -7,14 +7,16 @@ var mensaje2 = form.querySelector("#codigo");
 
 btnEncriptar.addEventListener("click", function (event) {
     event.preventDefault();
-    mensaje2.value = mensaje1.value;
+    var codificado = codificar(mensaje1.value);
+    mensaje2.value = codificado;
     mensaje1.value = "";
 });
 
 btnDesencriptar.addEventListener("click", function (event) {
     event.preventDefault();
-    mensaje1.value = mensaje2.value;
-    mensaje2.value = "";
+    var decodificado = decodificar(mensaje1.value);
+    mensaje2.value = decodificado;
+    mensaje1.value = "";
 });
 
 copiar.addEventListener("click", function (event) {
@@ -22,3 +24,46 @@ copiar.addEventListener("click", function (event) {
     mensaje2.select();
     document.execCommand('copy');
 });
+
+function codificar(mensaje){
+    var codigo = [];
+
+    for (var i = 0; i < mensaje.length; i++){
+        if (mensaje[i] == "a"){
+            codigo.push("ai")
+        } else if (mensaje[i] == "e"){
+            codigo.push("enter");
+        } else if (mensaje[i] == "i"){
+            codigo.push("imes");
+        } else if (mensaje[i] == "o"){
+            codigo.push("ober");
+        } else if (mensaje[i] == "u"){
+            codigo.push("ufat");
+        } else {
+            codigo.push(mensaje[i]);
+        }
+    }
+
+    return codigo.join("");
+}
+
+function decodificar(mensaje){
+    var codigo = "";
+
+    var a = mensaje.replace(/ai/g, "a");
+    codigo = codigo + a;
+
+    var e = codigo.replace(/enter/g, "e");
+    codigo = e;
+
+    var i = codigo.replace(/imes/g, "i");
+    codigo = i;
+
+    var o = codigo.replace(/ober/g, "o");
+    codigo = o;
+
+    var u = codigo.replace(/ufat/g, "u");
+    codigo = u;
+
+    return codigo;
+}
